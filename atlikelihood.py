81b0ac39075fd3738aafda178f34a797dc048d70
JVerbeek <janneke.verbeek@upcmail.nl>
2025-07-08 15:33:50 +0200

    Created custom likelihood

--- atlikelihood.py
+++ atlikelihood.py
@@ -2,22 +2,17 @@ import gpflow
 from gpflow.likelihoods import Likelihood
 from typing import Optional
 import tensorflow as tf
-from gpflow.likelihoods import ScalarLikelihood, MeanAndVariance
-from gpflow.utilities import TensorType, check_shapes, inherit_check_shapes
-from gpflow.utilities import inherit_check_shapes
-from gpflow.utilities import TensorLike, Sequence
+from gpflow.likelihoods import ScalarLikelihood
 
 class TransferLikelihood(ScalarLikelihood):
     """Copiloted the boilerplate for a custom likelihood class in GPflow."""
-    def __init__(self, likelihood_list: list[Likelihood], **kwargs):
+    def __init__(self, source: Likelihood, target: Likelihood, **kwargs):
         super().__init__(**kwargs)
-        self.likelihoods = list(likelihood_list)
+        self.source = source
+        self.target = target
+        self.likelihoods = [source, target]
 
-    @check_shapes(
-        "args[all]: [batch..., .]",
-        "return: [batch..., ...]",
-    )
-    def _partition_and_stitch(self, args: Sequence[TensorType], func_name: str) -> tf.Tensor:
+    def _partition_and_stitch(self, args, func_name: str) -> tf.Tensor:
         """
         args is a list of tensors, to be passed to self.likelihoods.<func_name>
 
@@ -47,36 +42,34 @@ class TransferLikelihood(ScalarLikelihood):
 
         return results
 
-    @inherit_check_shapes
-    def _scalar_log_prob(self, X: TensorType, F: TensorType, Y: TensorType) -> tf.Tensor:
+    def _scalar_log_prob(self, X, F, Y) -> tf.Tensor:
         return self._partition_and_stitch([X, F, Y], "_scalar_log_prob")
 
-    @inherit_check_shapes
     def _predict_log_density(
-        self, X: TensorType, Fmu: TensorType, Fvar: TensorType, Y: TensorType
+        self, X, Fmu, Fvar, Y
     ) -> tf.Tensor:
         return self._partition_and_stitch([X, Fmu, Fvar, Y], "predict_log_density")
 
-    @inherit_check_shapes
+
     def _variational_expectations(
-        self, X: TensorType, Fmu: TensorType, Fvar: TensorType, Y: TensorType
+        self, X, Fmu, Fvar, Y
     ) -> tf.Tensor:
         return self._partition_and_stitch([X, Fmu, Fvar, Y], "variational_expectations")
 
-    @inherit_check_shapes
+
     def _predict_mean_and_var(
-        self, X: TensorType, Fmu: TensorType, Fvar: TensorType
-    ) -> MeanAndVariance:
+        self, X, Fmu, Fvar
+    ):
         mvs = [lik.predict_mean_and_var(X, Fmu, Fvar) for lik in self.likelihoods]
         mu_list, var_list = zip(*mvs)
         mu = tf.concat(mu_list, axis=1)
         var = tf.concat(var_list, axis=1)
         return mu, var
 
-    @inherit_check_shapes
-    def _conditional_mean(self, X: TensorType, F: TensorType) -> tf.Tensor:
+
+    def _conditional_mean(self, X, F) -> tf.Tensor:
         raise NotImplementedError
 
-    @inherit_check_shapes
-    def _conditional_variance(self, X: TensorType, F: TensorType) -> tf.Tensor:
+
+    def _conditional_variance(self, X, F) -> tf.Tensor:
         raise NotImplementedError
